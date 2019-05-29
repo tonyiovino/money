d0a8a6cd53d035f8af965ed63d73aafc88db2f69
tonioiovino <antonioiovino2004tony@gmail.com>
2019-05-29 13:01:40 +0000

    Money

---
+++ money.py
@@ -0,0 +1,17 @@
+print("Inserisci importo: ")
+importo_str = input()
+importo = float(importo_str)
+
+while importo < 1
+	print("Per favore, inserire un numero almeno maggiore di 1: ")
+	importo_str = input()
+	importo = float(importo_str)
+
+resto = importo
+
+importo / 20
+importo % 20
+
+	print(importo)
+
+
