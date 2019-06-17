fa111942f628ef6f93f33f973f12582c332b007a
tonyiovino <antonioiovino2004tony@gmail.com>
2019-06-17 12:53:15 +0000

    Money

--- money.py
+++ money.py
@@ -2,11 +2,9 @@ print("Inserisci importo: ")
 importo_str = input()
 importo = float(importo_str)
 
-while importo < 1
-	print("Per favore, inserire un numero almeno maggiore di 1: ")
-	importo_str = input()
-	importo = float(importo_str)
-
+if importo < 0:
+	print("Per favore, inserisca un numero positivo!")
+	
 resto = importo
 
 importo / 20
