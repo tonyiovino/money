6a72d076094d9423d78b1ca57b02e86d588a7dba
tonyiovino <antonioiovino2004tony@gmail.com>
2019-06-17 12:58:19 +0000

    Money

--- money.py
+++ money.py
@@ -4,11 +4,12 @@ importo = float(importo_str)
 
 if importo < 0:
 	print("Per favore, inserisca un numero positivo!")
-	
-resto = importo
 
-importo / 20
-importo % 20
+else:
+	resto = importo
+
+	importo / 20
+	importo % 20
 
 	print(importo)
 
