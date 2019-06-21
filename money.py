9d923a6011664255b3ea0694ce79e175e508d810
tonyiovino <antonioiovino2004tony@gmail.com>
2019-06-21 18:06:02 +0000

    Money

--- money.py
+++ money.py
@@ -9,15 +9,21 @@ else:
 	dieci = 10
 	cinque = 5
 	uno = 1
-
+	
+	#Venti#
 	b_venti = importo // venti
-	importo 
-
-	#Il programma è in svolgimento...#
-
+	resto_venti = importo % venti
+	#Dieci#
+	resto_venti = importo
+	b_dieci = importo // dieci
+	resto_dieci = importo % dieci
+	#Cinque#
+	resto_dieci = importo
+	b_cinque = importo // cinque
+	resto_cinque = importo % cinque
+	#Uno#
+	resto_cinque = importo
+	b_uno = importo // uno
+	resto_uno = importo % uno
 	
-
-	
-	print(b_venti)
-
-
+	print("Biglietti da 20: ", b_venti,"\n" "Biglietti da 10: ", b_dieci,"\n" "Biglietti da 5: ", b_cinque,"\n" "Biglietti da 1: ", b_uno,"\n")
